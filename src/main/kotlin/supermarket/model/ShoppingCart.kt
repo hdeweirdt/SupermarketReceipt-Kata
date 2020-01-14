f11929332dedd3da3935424d2877733f97c4025a
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-01-14 16:56:25 +0100

    move calculation of discount to offer

--- src/main/kotlin/supermarket/model/ShoppingCart.kt
+++ src/main/kotlin/supermarket/model/ShoppingCart.kt
@@ -1,7 +1,6 @@
 package supermarket.model
 
 import supermarket.model.offer.Offer
-import supermarket.model.offer.SpecialOfferType
 
 class ShoppingCart {
 
@@ -21,63 +20,11 @@ class ShoppingCart {
 
     internal fun handleOffers(receipt: Receipt, offers: Map<Product, Offer>, catalog: SupermarketCatalog) {
         for (productQuantity in items) {
-            val quantity = productQuantity.quantity
             if (offers.containsKey(productQuantity.product)) {
-                val discount = calculateDiscount(offers, productQuantity, catalog, quantity)
+                val offer = offers[productQuantity.product]!!
+                val discount = offer.calculateDiscount(productQuantity, catalog)
                 receipt.addDiscount(discount)
             }
         }
-    }
-
-    private fun calculateDiscount(
-        offers: Map<Product, Offer>,
-        productQuantity: ProductQuantity,
-        catalog: SupermarketCatalog,
-        quantity: Double
-    ): Discount {
-        val offer = offers[productQuantity.product]!!
-        val unitPrice = catalog.getUnitPrice(productQuantity.product)
-        val quantityAsInt = quantity.toInt()
-        var productQuantityPerOfferApplication = 1
-        if (offer.offerType === SpecialOfferType.ThreeForTwo) {
-            productQuantityPerOfferApplication = 3
-
-        } else if (offer.offerType === SpecialOfferType.TwoForAmount) {
-            productQuantityPerOfferApplication = 2
-            if (quantityAsInt >= 2) {
-                val total =
-                    offer.argument * (quantityAsInt / productQuantityPerOfferApplication) + quantityAsInt % 2 * unitPrice
-                val discountN = unitPrice * quantity - total
-                return Discount(productQuantity.product, "2 for " + offer.argument, discountN)
-            }
-
-        }
-        if (offer.offerType === SpecialOfferType.FiveForAmount) {
-            productQuantityPerOfferApplication = 5
-        }
-        val timesToRepeatOffer = quantityAsInt / productQuantityPerOfferApplication
-        if (offer.offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-            val discountAmount =
-                quantity * unitPrice - (timesToRepeatOffer.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
-            return Discount(productQuantity.product, "3 for 2", discountAmount)
-        }
-        if (offer.offerType === SpecialOfferType.TenPercentDiscount) {
-            return Discount(
-                productQuantity.product,
-                offer.argument.toString() + "% off",
-                quantity * unitPrice * offer.argument / 100.0
-            )
-        }
-        if (offer.offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-            val discountTotal =
-                unitPrice * quantity - (offer.argument * timesToRepeatOffer + quantityAsInt % 5 * unitPrice)
-            return Discount(
-                productQuantity.product,
-                productQuantityPerOfferApplication.toString() + " for " + offer.argument,
-                discountTotal
-            )
-        }
-        // TODO: remove
-        return Discount(productQuantity.product, productQuantityPerOfferApplication.toString(), 0.0)
     }
 }
--- src/main/kotlin/supermarket/model/offer/Offer.kt
+++ src/main/kotlin/supermarket/model/offer/Offer.kt
@@ -1,8 +1,57 @@
 package supermarket.model.offer
 
+import supermarket.model.Discount
 import supermarket.model.Product
+import supermarket.model.ProductQuantity
+import supermarket.model.SupermarketCatalog
 
 class Offer(internal var offerType: SpecialOfferType, internal val product: Product, internal var argument: Double) {
+    fun calculateDiscount(productQuantity: ProductQuantity, catalog: SupermarketCatalog): Discount {
+        val quantity = productQuantity.quantity
+        val unitPrice = catalog.getUnitPrice(productQuantity.product)
+        val quantityAsInt = quantity.toInt()
+        var productQuantityPerOfferApplication = 1
+        if (offerType === SpecialOfferType.ThreeForTwo) {
+            productQuantityPerOfferApplication = 3
+
+        } else if (offerType === SpecialOfferType.TwoForAmount) {
+            productQuantityPerOfferApplication = 2
+            if (quantityAsInt >= 2) {
+                val total =
+                    argument * (quantityAsInt / productQuantityPerOfferApplication) + quantityAsInt % 2 * unitPrice
+                val discountN = unitPrice * quantity - total
+                return Discount(productQuantity.product, "2 for $argument", discountN)
+            }
+
+        }
+        if (offerType === SpecialOfferType.FiveForAmount) {
+            productQuantityPerOfferApplication = 5
+        }
+        val timesToRepeatOffer = quantityAsInt / productQuantityPerOfferApplication
+        if (offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+            val discountAmount =
+                quantity * unitPrice - (timesToRepeatOffer.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
+            return Discount(productQuantity.product, "3 for 2", discountAmount)
+        }
+        if (offerType === SpecialOfferType.TenPercentDiscount) {
+            return Discount(
+                productQuantity.product,
+                "$argument% off",
+                quantity * unitPrice * argument / 100.0
+            )
+        }
+        if (offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+            val discountTotal =
+                unitPrice * quantity - (argument * timesToRepeatOffer + quantityAsInt % 5 * unitPrice)
+            return Discount(
+                productQuantity.product,
+                "$productQuantityPerOfferApplication for $argument",
+                discountTotal
+            )
+        }
+        // TODO: remove this zero discount
+        return Discount(productQuantity.product, productQuantityPerOfferApplication.toString(), 0.0)
+    }
 
 }
 
